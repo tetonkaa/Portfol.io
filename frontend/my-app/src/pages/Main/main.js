d36e46c6ac4748a8e66f9678c085588fc16e81a0
Angel <angel.l.puente@gmail.com>
2023-01-27 14:08:34 -0600

    updated styling on main component

--- frontend/my-app/src/pages/Main/main.js
+++ frontend/my-app/src/pages/Main/main.js
@@ -9,9 +9,29 @@ export default function MainPage() {
     
     return (
         <>
-        <img className="headshot" src="https://cdn.dashfight.com/99d742ace4a0519f1b8a8ebf97bbd4f93362bae1.jpg" />
         <div className="MainPage">
+            <div className="profile">
+            <img className="headshot" src="https://cdn.dashfight.com/99d742ace4a0519f1b8a8ebf97bbd4f93362bae1.jpg" />
+                <div className="svgDiv">
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+                <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />                               
+                </div>
+            </div>
+
             <h1>Angel Puente</h1>
+
             <p className="blurb">Software Engineer with significant experience in the insurance and finance industry. Skilled in customer service, excelling through standardized work processes, and having an aptitude for learning new coding languages.</p>
 
             <div className="gitGraph">
@@ -27,24 +47,8 @@ export default function MainPage() {
                 username="tetonkaa">
             </GitHubCalendar>
             </div> 
-
         </div>
-            <div className="svgDiv">
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-            <img className="languageSvg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />                               
-            </div>
+
     </>
     );
 
